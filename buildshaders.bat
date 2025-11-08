e6cd96ff1b4b87f32da1c9df7fe999db8025a9d2
#xoxor4d <r4d0xzz@live.de>
2025-11-08 13:36:37 +0100

    bat: compile remix shaders + create final directories if they do not exist
    created 4kb file with no file ext. when dir does not exist

--- buildshaders.bat
+++ buildshaders.bat
@@ -1,15 +1,26 @@
 @echo on
 set shaders_path=%cd%/win32_30_nv8
+set shaders_remix_path=%cd%/win32_30_remix
 set win32_30=%cd%/update/common/shaders/win32_30
 set win32_30_atidx10=%cd%/update/common/shaders/win32_30_atidx10
-set win32_30_nv8=%cd%/update/common/shaders/win32_30_nv8
 
 for /R "%shaders_path%" %%a in (*.xml) do (
     RageShaderEditor.exe "%%a"
 )
 
+if not exist "%win32_30%" mkdir "%win32_30%"
 for /R "%shaders_path%" %%i in (*.fxc) do (
 echo D | xcopy "%%i" "%win32_30%" /K /H /Y
+del "%%i"
+)
+
+for /R "%shaders_remix_path%" %%a in (*.xml) do (
+    RageShaderEditor.exe "%%a"
+)
+
+if not exist "%win32_30_atidx10%" mkdir "%win32_30_atidx10%"
+for /R "%shaders_remix_path%" %%i in (*.fxc) do (
+echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y
 del "%%i"
 )
 
